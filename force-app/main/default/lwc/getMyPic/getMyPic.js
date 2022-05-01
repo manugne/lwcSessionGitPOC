2df8b8d0c16d6319e9bf35693082c16b7828fe32
manugne@gmail.com <manugne@gmail.com>
2022-05-01 23:38:46 +0530

    Latest Commit

--- force-app/main/default/lwc/getMyPic/getMyPic.js
+++ force-app/main/default/lwc/getMyPic/getMyPic.js
@@ -4,6 +4,7 @@ export default class GetMyPic extends LightningElement {
   @api recordId;
 
   get picUrl() {
+    //Comment Manu 1
     return `https://i.pravatar.cc/300?u=${this.recordId}`;
   }
 }
